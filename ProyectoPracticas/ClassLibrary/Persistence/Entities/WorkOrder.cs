5d007482ea67e63c5373c42a99da73729314de03
Lluis Porcar Ruiz <LPORRUI@upv.edu.es>
2025-11-19 10:55:30 +0100

    solo pull (espere)

--- ProyectoPracticas/ClassLibrary/Persistence/Entities/WorkOrder.cs
+++ ProyectoPracticas/ClassLibrary/Persistence/Entities/WorkOrder.cs
@@ -27,5 +27,7 @@ namespace ManteHos.Entities
         public virtual ICollection<UsedPart> UsedParts { get; set; }
 
         public virtual ICollection<Operator> Operators { get; set; }
+
+        //public virtual ICollection<WorkOrder> WorkOrders { get; set; }
     }
 }
