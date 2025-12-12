87b6f2c4d2532d7f3b4e1e572f7ac25fee8fa575
Lluis Porcar Ruiz <LPORRUI@upv.edu.es>
2025-12-12 10:50:43 +0100

    Fent AssingWorkOrder

--- ProyectoPracticas/ManteHosGUI/AssingWorkOrderFrom.Designer.cs
+++ ProyectoPracticas/ManteHosGUI/AssingWorkOrderFrom.Designer.cs
@@ -42,20 +42,23 @@
             this.dgvIncidents.AllowUserToAddRows = false;
             this.dgvIncidents.AllowUserToDeleteRows = false;
             this.dgvIncidents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvIncidents.Location = new System.Drawing.Point(12, 12);
+            this.dgvIncidents.Location = new System.Drawing.Point(16, 15);
+            this.dgvIncidents.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvIncidents.MultiSelect = false;
             this.dgvIncidents.Name = "dgvIncidents";
             this.dgvIncidents.ReadOnly = true;
+            this.dgvIncidents.RowHeadersWidth = 51;
             this.dgvIncidents.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvIncidents.Size = new System.Drawing.Size(399, 211);
+            this.dgvIncidents.Size = new System.Drawing.Size(532, 260);
             this.dgvIncidents.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(87, 293);
+            this.label1.Location = new System.Drawing.Point(144, 363);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.Size = new System.Drawing.Size(82, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Seleccionar:";
             // 
@@ -63,26 +66,29 @@
             // 
             this.cbOperators.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbOperators.FormattingEnabled = true;
-            this.cbOperators.Location = new System.Drawing.Point(159, 290);
+            this.cbOperators.Location = new System.Drawing.Point(240, 359);
+            this.cbOperators.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbOperators.Name = "cbOperators";
-            this.cbOperators.Size = new System.Drawing.Size(121, 21);
+            this.cbOperators.Size = new System.Drawing.Size(160, 24);
             this.cbOperators.TabIndex = 2;
             // 
             // btnQuit
             // 
-            this.btnQuit.Location = new System.Drawing.Point(56, 368);
+            this.btnQuit.Location = new System.Drawing.Point(103, 455);
+            this.btnQuit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnQuit.Name = "btnQuit";
             this.btnQuit.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btnQuit.Size = new System.Drawing.Size(113, 23);
+            this.btnQuit.Size = new System.Drawing.Size(151, 28);
             this.btnQuit.TabIndex = 3;
             this.btnQuit.Text = "Retirar Operari";
             this.btnQuit.UseVisualStyleBackColor = true;
             // 
             // btnAssign
             // 
-            this.btnAssign.Location = new System.Drawing.Point(204, 368);
+            this.btnAssign.Location = new System.Drawing.Point(300, 455);
+            this.btnAssign.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAssign.Name = "btnAssign";
-            this.btnAssign.Size = new System.Drawing.Size(113, 23);
+            this.btnAssign.Size = new System.Drawing.Size(151, 28);
             this.btnAssign.TabIndex = 4;
             this.btnAssign.Text = "Asignar Operari";
             this.btnAssign.UseVisualStyleBackColor = true;
@@ -90,25 +96,28 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(92, 252);
+            this.label2.Location = new System.Drawing.Point(151, 312);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(188, 13);
+            this.label2.Size = new System.Drawing.Size(237, 16);
             this.label2.TabIndex = 5;
             this.label2.Text = "Asignar operari al incident seleccionat:";
             // 
             // AssingWorkOrderFrom
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 450);
+            this.ClientSize = new System.Drawing.Size(562, 547);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnAssign);
             this.Controls.Add(this.btnQuit);
             this.Controls.Add(this.cbOperators);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvIncidents);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AssingWorkOrderFrom";
             this.Text = "AssingWorkOrderFrom";
+            this.Load += new System.EventHandler(this.AssingWorkOrderFrom_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvIncidents)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- ProyectoPracticas/ManteHosGUI/AssingWorkOrderFrom.cs
+++ ProyectoPracticas/ManteHosGUI/AssingWorkOrderFrom.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ManteHos.Services;
+using ManteHos.Entities;
 
 namespace ManteHosGUI
 {
     public partial class AssingWorkOrderFrom : Form
     {
-        public AssingWorkOrderFrom()
+        private IManteHosService service;
+
+        public AssingWorkOrderFrom(IManteHosService service)
         {
             InitializeComponent();
+            this.service = service;
         }
+
+        private void CargarOperators()
+        {
+            cbOperators.DataSource = service.GetAllOperators();
+            cbOperators.DisplayMember = "FullName";
+            cbOperators.ValueMember = "Id";
+        }
+
+        private void CargarIncidents()
+        {
+            var incidents = service.GetIncidentsMaster();
+
+            var gridData = incidents.Select(i => new
+            {
+                Id = i.Id,
+                Description = i.Description,
+                Priority = i.Priority,
+                Status = i.Status,
+
+                asignedOperators = (i.WorkOrder != null && i.WorkOrder.Operators.Count > 0)
+                    ? string.Join(",", i.WorkOrder.Operators.Select(o => o.FullName))
+                    : "Sense Asignar"
+            }).ToList();
+
+            dgvIncidents.DataSource = gridData;
+        }
+
+        private void AssingWorkOrderFrom_Load(object sender, EventArgs e)
+        {
+            CargarIncidents();
+            CargarOperators();
+        }
+
     }
 }
